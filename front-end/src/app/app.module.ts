67f4ce12f5c7017254af95d7e8e7e57b3489aed5
Rioedev <lequy11082003@gmail.com>
2023-05-14 08:24:21 +0700

    tags bar

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { RatingModule } from 'ng-starrating';
 import { SearchComponent } from './components/partials/search/search.component';
 import { FoodPageComponent } from './components/pages/food-page/food-page.component';
+import { TagsComponent } from './components/partials/tags/tags.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, FoodPageComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, FoodPageComponent, TagsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
