cd1533e139df9126ee632f324feb7cccb28bd367
Rioedev <lequy11082003@gmail.com>
2023-05-14 10:41:30 +0700

    add to cart (xử lý giao diện)

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -10,9 +10,11 @@ import { SearchComponent } from './components/partials/search/search.component';
 import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { TitleComponent } from './components/partials/title/title.component';
+import { NotFoundComponent } from './components/partials/not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, FoodPageComponent, TagsComponent, CartPageComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, FoodPageComponent, TagsComponent, CartPageComponent, TitleComponent, NotFoundComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
